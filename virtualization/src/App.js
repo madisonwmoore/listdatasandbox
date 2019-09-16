5dace749e76f367c0336c40d0fc2c0edf8fb6d5c
Madison <madisonwmoore@gmail.com>
2019-09-16 12:37:38 -0500

    styling updates

--- virtualization/src/App.js
+++ virtualization/src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { loremIpsum } from "lorem-ipsum";
-import { List } from "react-virtualized";
+import { List,AutoSizer } from "react-virtualized";
+import faker from 'faker/locale/en_US';
 
-const rowCount=1000000;
+const rowCount=10000;
 
 class App extends React.Component {
 constructor(){
   super();
+ 
+  this.state={
+    name:""
+  }
   this.list=Array(rowCount).fill().map((val,idx)=>{
+    
     return{
       id:idx,
-      name:'John Doe',
+      city:faker.address.city(),
+      name:faker.name.findName(),
       image:'http://via.placeholder.com/40',
       text: loremIpsum({
         count: 1,
@@ -27,12 +34,12 @@ constructor(){
 renderRow=({ index, key, style })=> {
 
   return (
-    <div key={key} style={style} className="row">
+    <div onClick={()=>{this.setState({name:this.list[index].city})}} key={key} style={style} className="row">
       <div className="image">
         <img src={this.list[index].image} alt="" />
       </div>
       <div className="content">
-        <div>{this.list[index].name}</div>
+        <div>{this.list[index].city}</div>
         <div>{this.list[index].text}</div>
       </div>
     </div>
@@ -47,8 +54,24 @@ renderRow=({ index, key, style })=> {
     const listHeight=600;
     return (
     <div className="App">
+      <div className="">
+      <h1>Large Data Front End Demo</h1>
+      </div>
+      <div className="grid-container">
      <div className="list">
-      <List width={rowWidth} height={listHeight} rowHeight={rowHeight} rowRenderer={this.renderRow} rowCount={this.list.length}/>
+
+<AutoSizer>
+{({width,height}) => (
+      <List width={width} height={height} rowHeight={rowHeight} rowRenderer={this.renderRow} rowCount={this.list.length}/>
+)}
+</AutoSizer>
+
+     </div>
+     <div className="details">
+     
+       <h1 id="name">Name:{this.state.name}</h1>
+  
+     </div> 
      </div>
     </div>
   );
